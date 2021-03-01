e5f78f3b69a7091f18b2eb2e0cb5b7008606f8bb
Rotem Matityahu <>
2021-03-01 18:08:40 +0200

    change package name in sdk manifest

--- foregroundsdk/src/main/java/com/rotemati/foregroundsdk/internal/notification/DefaultNotificationCreator.kt
+++ foregroundsdk/src/main/java/com/rotemati/foregroundsdk/internal/notification/DefaultNotificationCreator.kt
@@ -4,18 +4,16 @@ import android.app.Notification
 import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import com.rotemati.foregroundtesterapp.sdk.R
 
 internal class DefaultNotificationCreator {
 	fun create(context: Context): Notification {
-		val resources = context.resources
-		val channelId = resources.getString(R.string.default_foreground_notification_channel_id)
+		val channelId = "Process updates"
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 			NotificationChannelsCreator(context).createChannel(channelId)
 		}
 		return NotificationCompat.Builder(context, channelId)
-				.setContentTitle(resources.getString(R.string.default_foreground_notification_title))
-				.setContentText(resources.getString(R.string.default_foreground_notification_body))
+				.setContentTitle("Process updates")
+				.setContentText("Initializing the experience")
 				.setSmallIcon(android.R.drawable.stat_notify_sync)
 				.build()
 	}
